76bafc0a95715cbbe63b2a0f447abbd3eab5ccde
jeevajeyakrishnan <jeeevasuriya@gmail.com>
2025-01-28 07:51:16 +0530

    Password generated

---
+++ CodePlayApp/CodePlayApp/GeneratePass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePlayApp
+{
+    public class GeneratePass
+    {
+        public void GeneratePassword()
+        {
+            Random rand = new Random();
+
+            string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int charsize = 3;
+            string Number = "1234567890";
+            string SpeChar = "!@#$%^&*()";
+            int size = 1;
+            string randpasss = "";
+
+                int small = rand.Next(Lowercase.Length);
+                int caps = rand.Next(Uppercase.Length);
+                randpasss += Lowercase[small];
+                randpasss += Uppercase[caps];
+
+          
+
+            for (int i = 0; i < size; i++)
+            {
+
+                int num = rand.Next(Number.Length);
+                int spechar = rand.Next(SpeChar.Length);
+
+                randpasss += Number[num];
+                randpasss += SpeChar[spechar];
+
+            }
+            Console.WriteLine(randpasss);
+        }
+    }
+}
